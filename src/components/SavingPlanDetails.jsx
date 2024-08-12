40b5fc5488665644ee36131d3537fcc71020fb3a
sachin2398 <smartsachin38@gmail.com>
2024-08-12 16:53:26 +0530

    order

--- src/components/SavingPlanDetails.jsx
+++ src/components/SavingPlanDetails.jsx
@@ -5,12 +5,7 @@ const SavingPlanDetails = ({ openSidebar }) => {
     <div className='saving-plan-details flex justify-between'>
       <h2 className='text-2xl text-[#2C2C2C]'>Saving plan details</h2>
       <div className='plan-actions flex gap-3'>
-        <button
-          onClick={() => openSidebar('upcoming-orders')}
-          className='px-6 py-3 border rounded-2xl hover:bg-green-700 hover:text-white transition-colors duration-300'
-        >
-          Upcoming orders
-        </button>
+       
         <button
           onClick={() => openSidebar('order-history')}
           className='px-6 py-3 border rounded-2xl hover:bg-green-700 hover:text-white transition-colors duration-300'
